72b17b2982125492cf2dfc1f4fdfa83bd9a32edd
Victor Sun <vtsun@email.unc.edu>
2024-04-29 22:43:41 -0400

    Add chart to transactioncomponent

--- frontend/src/TransactionsComponent.js
+++ frontend/src/TransactionsComponent.js
@@ -1,32 +1,207 @@
+// import React, { Component, useEffect, useRef } from 'react';
+// import Chart from 'chart.js/auto';
+
+// import './App.css';
+
+// class TransactionsComponent extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.chartRef = React.createRef();
+//         this.chart = null; // Store reference to the chart instance
+
+//     }
+
+//     componentDidMount() {
+//         this.buildChart();
+//     }
+
+//     componentWillUnmount() {
+//         if (this.chart) {
+//             this.chart.destroy(); // Destroy the chart instance when the component unmounts
+//         }
+//     }
+
+//     buildChart() {
+//         const ctx = this.chartRef.current.getContext('2d');
+//         if (this.chart) {
+//             this.chart.destroy(); // Destroy previous chart instance if exists
+//         }
+//         new Chart(ctx, {
+//             type: 'line',
+//             data: {
+//                 labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+//                 datasets: [{
+//                     label: 'Transaction Amount',
+//                     data: [1, 2, 3, 4, 5, 6, 7],
+//                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
+//                     borderColor: 'rgba(54, 162, 235, 1)',
+//                     borderWidth: 1
+//                 }]
+//             },
+//             options: {
+//                 scales: {
+//                     y: {
+//                         beginAtZero: true,
+//                         title: {
+//                             display: true,
+//                             text: 'Amount ($)'
+//                         }
+//                     },
+//                     x: {
+//                         title: {
+//                             display: true,
+//                             text: 'Day of the Week'
+//                         }
+//                     }
+//                 }
+//             }
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Transactions</h3>
+//                 <div>
+//                     <canvas ref={this.chartRef} width='400' height='200'></canvas>
+//                 </div>
+
+//                 <div className="TransactionsTotalsContainer">
+//                     <div className="TransactionBox">
+//                         <h3>Total Expenses</h3>
+//                         <div>
+//                             <p id='TotalExpenses'>
+//                                 $0
+//                             </p>
+//                         </div>
+//                     </div>
+//                     <div className="TransactionBox">
+//                         <h3>Total Income</h3>
+//                         <div>
+//                             <p id='TotalIncome'>
+//                                 $0
+//                             </p>
+//                         </div>
+//                     </div>
+//                     <div className="TransactionBox">
+//                         <h3>Balance</h3>
+//                         <div>
+//                             <p id='Balance'>
+//                                 $0
+//                             </p>
+//                         </div> {/* Replace this with your dynamic data */}
+//                     </div>
+//                 </div>
+
+//                 <div>
+//                     Select Currency
+//                     <select>
+//                         <option value='USD'>USD</option>
+//                         <option value='Euro'>Euro</option>
+//                         <option value='Pound'>Pound</option>
+//                         <option value='Yen'>Yen</option>
+//                     </select>
+//                 </div>
+//             </div>
+//         )
+//     }
+
+// }
+
+// export default TransactionsComponent;
+
 import React from 'react';
+import Chart from 'chart.js/auto';
 import './App.css';
 
 class TransactionsComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.chartRef = React.createRef();
+        this.chart = null; // Store reference to the chart instance
+    }
+
+    componentDidMount() {
+        this.buildChart();
+    }
+
+    componentWillUnmount() {
+        if (this.chart) {
+            this.chart.destroy(); // Destroy the chart instance when the component unmounts
+        }
+    }
+
+    buildChart() {
+        const ctx = this.chartRef.current.getContext('2d');
+        if (this.chart) {
+            this.chart.destroy(); // Destroy previous chart instance if exists
+        }
+        this.chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                datasets: [{
+                    label: 'Transaction Amount',
+                    data: [1, 2, 3, 4, 5, 6, 7],
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'Amount ($)'
+                        }
+                    },
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Day of the Week'
+                        }
+                    }
+                }
+            }
+        });
+    }
+
     render() {
         return (
             <div>
                 <h3>Transactions</h3>
                 <div>
-                    {/* Chart */}
-
+                    <canvas ref={this.chartRef} width='400' height='200'></canvas>
                 </div>
 
                 <div className="TransactionsTotalsContainer">
-                    <div className="Box">
+                    <div className="TransactionBox">
                         <h3>Total Expenses</h3>
-                        <div>$1000</div> {/* Replace this with your dynamic data */}
+                        <div>
+                            <p id='TotalExpenses'>
+                                $0
+                            </p>
+                        </div>
                     </div>
-                    <div className="Box">
+                    <div className="TransactionBox">
                         <h3>Total Income</h3>
-                        <div>$2000</div> {/* Replace this with your dynamic data */}
+                        <div>
+                            <p id='TotalIncome'>
+                                $0
+                            </p>
+                        </div>
                     </div>
-                    <div className="Box">
+                    <div className="TransactionBox">
                         <h3>Balance</h3>
-                        <div>$1000</div> {/* Replace this with your dynamic data */}
+                        <div>
+                            <p id='Balance'>
+                                $0
+                            </p>
+                        </div> {/* Replace this with your dynamic data */}
                     </div>
                 </div>
-
-
 
                 <div>
                     Select Currency
@@ -37,11 +212,9 @@ class TransactionsComponent extends React.Component {
                         <option value='Yen'>Yen</option>
                     </select>
                 </div>
-
             </div>
         )
     }
-
 }
 
 export default TransactionsComponent;
